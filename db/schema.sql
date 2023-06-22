3d4833613cdf219ec0c0ae39804b90d3461ec921
unknown <john.gordos@outlook.com>
2023-06-22 16:32:30 -0600

    Updated schema to require a department for roles, auto incremented employee id

--- db/schema.sql
+++ db/schema.sql
@@ -12,20 +12,20 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
-  department_id INT,
+  department_id INT NOT NULL,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
-  ON DELETE SET NULL
+  ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   FOREIGN KEY (role_id)
   REFERENCES role(id)
-  ON DELETE SET NULL,
+  ON DELETE CASCADE,
   manager_id INT,
   FOREIGN KEY (manager_id)
   REFERENCES employee(id)
--- db/seed.sql
+++ db/seed.sql
@@ -2,9 +2,9 @@ INSERT into department(name)
 VALUES ("Sales"), ("Service");
 
 INSERT into role(title, salary, department_id)
-VALUES ("Sales Development Rep", 10, 0), ("Sales Rep", 20, 0), ("Sales Manager", 30, 0),
-("Support Agent", 10, 1), ("Senior Support Agent", 20, 1), ("Support Manager", 66, 1);
+VALUES ("Sales Development Rep", 10, 1), ("Sales Rep", 20, 1), ("Sales Manager", 30, 1),
+("Support Agent", 10, 2), ("Senior Support Agent", 20, 2), ("Support Manager", 66, 2);
 
-INSERT into employee(id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Bolin", "Smol", 3, null), (2, "Aaron", "Rodgerz", 2, 1), (3, "Michelle", "Deux", 2, 1), (4, "Tyr", "Schorson", 1, 1),
-(5, "Bruno", "Salzar", 6, null), (6, "Tammy", "Fring", 5, 5), (7, "Jahnny", "G-Unit", 5, 5), (8, "Dakota", "Moore", 4, 6);
+INSERT into employee(first_name, last_name, role_id, manager_id)
+VALUES ("Bolin", "Smol", 3, null), ("Aaron", "Rodgerz", 2, 1), ("Michelle", "Deux", 2, 1), ("Tyr", "Schorson", 1, 1),
+("Bruno", "Salzar", 6, null), ("Tammy", "Fring", 5, 5), ("Jahnny", "G-Unit", 5, 5), ("Dakota", "Moore", 4, 6);
